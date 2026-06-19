094bf77b194acbb434c67610bd8069f7143a9b0c
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-19 16:31:10 +0700

    fmt.Scan(&...)/NewScanner(os.Stdin)

--- lesson01/app.go
+++ lesson01/app.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	var hoten string;
 	fmt.Print("Nhap ho va ten: ");
+	//fmt.Scanln(&hoten);
 	scanner := bufio.NewScanner(os.Stdin);
 	if (scanner.Scan()) {
 	hoten = scanner.Text();
