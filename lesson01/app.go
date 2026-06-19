bd378e54e474c5df8abbfc4438d2ba9dce14c40a
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-19 16:29:31 +0700

    fmt.scan()/NewScanner(os.Stdin)

--- lesson01/app.go
+++ lesson01/app.go
@@ -1,18 +1,16 @@
 package main
-import "fmt"
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 func main() {
-	// fmt.Println("Hello World")
-	// randomUser();
-	var fullName string = "Nguyễn Phúc Thịnh";
-	fmt.Println(fullName);
-	var age int;
-	// Nếu không khai báo giá trị thì mặc định bằng 0
-	age=20;
-	fmt.Println(age);
-	// := khai báo giá trị ko cần khai báo kiểu
-	//Lưu ý: chỉ dùng được trong 1 function
-	phone := "0123456789";
-	fmt.Println(phone);
-	toan, van, anh := 10, 20, 30;
-	fmt.Println(toan, van, anh);
+	var hoten string;
+	fmt.Print("Nhap ho va ten: ");
+	scanner := bufio.NewScanner(os.Stdin);
+	if (scanner.Scan()) {
+	hoten = scanner.Text();
+	}
+	fmt.Println("Xin chao: ", hoten);
 }
