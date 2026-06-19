5cc7ffc0a67a157881328df8b60efca471d7877b
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-19 16:14:55 +0700

    Short Variables Declaration

--- lesson01/app.go
+++ lesson01/app.go
@@ -1,6 +1,18 @@
 package main
 import "fmt"
 func main() {
-	fmt.Println("Hello World")
-	randomUser();
+	// fmt.Println("Hello World")
+	// randomUser();
+	var fullName string = "Nguyễn Phúc Thịnh";
+	fmt.Println(fullName);
+	var age int;
+	// Nếu không khai báo giá trị thì mặc định bằng 0
+	age=20;
+	fmt.Println(age);
+	// := khai báo giá trị ko cần khai báo kiểu
+	//Lưu ý: chỉ dùng được trong 1 function
+	phone := "0123456789";
+	fmt.Println(phone);
+	toan, van, anh := 10, 20, 30;
+	fmt.Println(toan, van, anh);
 }
